f20acc98004cc71d6d98a8de8f7f9a261d825fd1
xiaohuoni <448627663@qq.com>
2020-04-01 17:47:11 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'ant-design-atom',
+  description: '业务组件',
+});
---
+++ src/components/Demo/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Page: FC = () => {
+  return <div>1</div>;
+};
+
+export default Page;
