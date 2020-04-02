263e109c2deaf3a12af19449f38ccc6c1d1adcba
xiaohuoni <448627663@qq.com>
2020-04-02 11:25:20 +0800

    feat: add pro

--- .umirc.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'dumi';
-
-export default defineConfig({
-  title: 'ant-design-atom',
-  description: '业务组件',
-});
--- src/components/Demo/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-
-const Page: FC = () => {
-  return <div>1</div>;
-};
-
-export default Page;
