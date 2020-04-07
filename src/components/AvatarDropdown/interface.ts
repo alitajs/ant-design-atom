0c719d55e4f52f890b3b41b7cd162813a3e3157c
chenshuhang <437132124@qq.com>
2020-04-07 11:15:17 +0800

    feat: delete components umi

---
+++ src/components/AvatarDropdown/interface.ts
@@ -0,0 +1,13 @@
+export interface CurrentUser {
+  avatar?: string;
+  name?: string;
+  title?: string;
+  group?: string;
+  signature?: string;
+  tags?: {
+    key: string;
+    label: string;
+  }[];
+  userid?: string;
+  unreadCount?: number;
+}
